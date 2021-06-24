f3cdddc70bd09cf4205ac5f39c9d4ab8aa125884
Naveen Honest Raj K <naveendurai19@gmail.com>
2021-06-24 04:56:17 -0700

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link href="/favicon.ico" rel="shortcut icon" />
+        <link href="/site.webmanifest" rel="manifest" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta property="og:title" content={title || `Naveen Honest Raj | Ruby on Rails Dev | Personal Site`} />
         <meta property="og:site_name" content="Telmo, code & design" />
