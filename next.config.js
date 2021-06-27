068d36bf1d2b4f09fc4faef95ff8a2739cbe3165
Naveen Honest Raj K <naveendurai19@gmail.com>
2021-06-27 00:16:43 -0700

    terser-webpack-plugin is added

--- next.config.js
+++ next.config.js
@@ -3,26 +3,26 @@ const withCSS = require('@zeit/next-css');
 const withTM = require('next-transpile-modules');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin')
+const withPurgeCss = require('next-purgecss')
+
 
 
 module.exports = withCSS(
-	withSass(
-		withTM({
-  		transpileModules: [
-  			'react-flexbox-grid',
-  			'react-syntax-highlighter',
-  		],
-  		webpack: function(config) {
-		    config.module.rules.push({
-		      test: /\.md$/,
-		      use: 'raw-loader',
-		    })
-				config.optimization.minimizer = [];
-				config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
-		    config.optimization.minimizer.push(new TerserPlugin());
+	withTM({
+		transpileModules: [
+			'react-flexbox-grid',
+			'react-syntax-highlighter',
+		],
+		webpack: function(config) {
+	    config.module.rules.push({
+	      test: /\.md$/,
+	      use: 'raw-loader',
+	    })
+			config.optimization.minimizer = [];
+			config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+	    config.optimization.minimizer.push(new TerserPlugin());
 
-		    return config
-		  },
-		})
-	),
+	    return config
+	  },
+	})
 );
