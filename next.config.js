274c4d4d59eeb784c359c8b7d5e0afeed0ad3dc6
Naveen Honest Raj K <naveendurai19@gmail.com>
2021-06-27 00:05:28 -0700

    terser-webpack-plugin is added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 const withTM = require('next-transpile-modules');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin')
+
 
 module.exports = withCSS(
 	withSass(
@@ -17,6 +19,8 @@ module.exports = withCSS(
 		    })
 				config.optimization.minimizer = [];
 				config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+		    config.optimization.minimizer.push(new TerserPlugin());
+
 		    return config
 		  },
 		})
