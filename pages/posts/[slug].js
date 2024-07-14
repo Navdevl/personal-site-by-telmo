059ffc2e470d4f6a6b02f3dda31b06f7c3eb9871
Naveen Honest Raj K <naveendurai19@gmail.com>
2024-07-15 01:09:33 +0530

    Author pict added

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,18 +1,19 @@
-import React from 'react'
 import matter from 'gray-matter'
-import ReactMarkdown from 'react-markdown'
 import Link from 'next/link'
+import React from 'react'
 import { BookOpen } from 'react-feather'
+import ReactMarkdown from 'react-markdown'
 import ProgressBar from 'react-scroll-progress-bar'
 import readingTime from 'reading-time'
 
-import Layout from '../../components/Layout'
 import CodeBlock from '../../components/CodeBlock'
+import Layout from '../../components/Layout'
 
 function Writing({ content, data }) {
   const frontmatter = data
   const { title, author } = frontmatter
-  const avatar = `https://images.weserv.nl/?url=https://unavatar.now.sh/twitter/${author.twitter}&w=40`
+  // const avatar = `https://images.weserv.nl/?url=https://unavatar.now.sh/twitter/${author.twitter}&w=120`
+  const avatar = `https://unavatar.io/x/${author.twitter}?w=120`
   const { text } = readingTime(content)
 
   return (
@@ -51,7 +52,7 @@ function Writing({ content, data }) {
                 code: CodeBlock,
                 link: props => {
                   if (!props.href.startsWith('http')) {
-                     return <a href={props.href} rel="nofollow noreferrer noopener">{props.children}</a>;
+                    return <a href={props.href} rel="nofollow noreferrer noopener">{props.children}</a>;
                   }
 
                   return <a href={props.href} rel="nofollow noreferrer noopener" target="_blank">{props.children}</a>;
